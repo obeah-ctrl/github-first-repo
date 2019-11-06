4e995295c91e586402d080e642a0ff8065947c7e
obeah-ctrl <57318229+obeah-ctrl@users.noreply.github.com>
2019-11-06 12:23:03 +0545

    Add files via upload

---
+++ templateliterals.js
@@ -0,0 +1,17 @@
+const message = 'This is my\n first message';
+
+const another = `This is my
+first message
+'hello'
+'hello'`;
+
+//usually for email.
+
+// ``this is a template literals
+
+//placeholders
+
+const name = 'John';
+const newMessage = `Hi ${name} ${2+3},
+
+whats up`; 
